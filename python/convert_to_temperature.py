6747044697b5f491904ad656962f014a4bc1d4c6
Kevin Bandura <kbandura@gmail.com>
2017-07-28 11:15:28 -0400

    added gain to convert to temperature

--- python/convert_to_temperature.py
+++ python/convert_to_temperature.py
@@ -19,14 +19,19 @@ if __name__ == '__main__':
     hot = fgd['spectrum'][0,:]
     Y = hot/cold #in linear ratio.  otherwise:Y = 10**((hot-cold)/10.0)
     T_sys = (300.0 - Y*10.0)/(Y-1)  #10 is assumed "cold sky" temperature 300gnd
+    gain = cold/(10.0 + T_sys)
     fstart = fgal.attrs['freq_start']
     fstep = fgal.attrs['freq_step']
     flength = cold.shape[0]
     freq = np.arange(flength)*fstep + fstart
-    np.savetxt((args.output_file[:-4]+".csv"), T_sys, delimiter="," )
+    np.savetxt((args.output_file[:-4]+"_Tsys.csv"), T_sys, delimiter="," )
+    np.savetxt((args.output_file[:-4]+"_gain.csv"), gain, delimiter="," )
     pylab.rcParams['axes.formatter.useoffset'] = False
     pylab.plot(T_sys)
     pylab.ylim(0,600)
-    pylab.savefig(args.output_file)
+    pylab.savefig(args.output_file[:-4]+"_noise.pdf")
+    pylab.clf()
+    pylab.plot(gain)
+    pylab.savefig(args.output_file[:-4] + "_gain.pdf")
     print("DONE SAVING TEMPERATURE.")
 
